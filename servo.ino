6e43ad940d6c785a218c2442c212cb6bf57dc8a8
Juan Molinas <juanevelio@gmail.com>
2018-11-03 22:37:32 -0300

    Update servo.ino

--- servo.ino
+++ servo.ino
@@ -1,7 +1,7 @@
  
 #include <Servo.h> 
 // Incluímos la librería servo.h
-
+# define tiempo_1 10;
 
 Servo servoMotor;
 // Declaramos la variable para controlar el servo
@@ -9,7 +9,8 @@ Servo servoMotor;
 
 //funcion de configuraciones e inicializacion
 void setup() {
-  // Iniciamos el monitor serie en 9600 baudios
+  /*Iniciamos el monitor serie en 9600 baudios La tasa de baudios (en inglés baud rate)
+ ―también conocida como baudaje― es el número de unidades de señal por segundo.*/
   Serial.begin(9600);
 
   // Iniciamos el servo en el pin 9
@@ -32,4 +33,23 @@ void loop() {
   servoMotor.write(180);
   // Esperamos 1 segundo
   delay(1000);
+ 
+ // Vamos a tener dos bucles uno para mover en sentido positivo y otro en sentido negativo
+  // Para el sentido positivo
+  for (int i = 0; i <= 180; i++)
+  {
+    // Desplazamos al ángulo correspondiente
+    servoMotor.write(i);
+    // Hacemos una pausa de "tiempo_1 milisegundos"
+    delay(tiempo_1);
+  }
+ 
+  // Para el sentido negativo
+  for (int i = 179; i > 0; i--)
+  {
+    // Desplazamos al ángulo correspondiente
+    servoMotor.write(i);
+   // Hacemos una pausa de "tiempo_1 milisegundos"
+    delay(tiempo_1);
+  }
 }
