044bf29c4006e68c00f4f9d2074c9eece19a968a
Juan Molinas <juanevelio@gmail.com>
2018-11-05 12:55:12 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
+const serialport= require("serialport");
+const parser= serialport.parsers.Readline// readline para tener cada lectura en una nueva linea 
+const puerto = new serialport("COM3",{
+    baudRate:9600,
+})//inicializamos desde donde estiramos la lectura
 
+const lectura = puerto.pipe(new parser({ delimeter:"\r\n"})) //pipe: tubo en En
+    lectura.on("open",function()
+    {
+        console.log("conectado!")
+    })
+
+lectura.on("data",function(data){
+    console.log(data)
+})
+puerto.on("error",function(err){
+    console.log(err)
+})
