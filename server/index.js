7c75063247e6cdaad2f953f5c96d5046eb4bc3b7
Juan Molinas <juanevelio@gmail.com>
2018-11-05 15:41:25 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
+
+
+const express = require("express")
+const app = express()
+const http = require("http")
+const socketIO= require("socket.io")
+const servidor =http.createServer(app)
+const io = socketIO.listen(servidor)
+servidor.listening(3000, function(){
+    console.log("servidor escuchando en puerto", 3000)
+})
+
+
+
+
+/*
+---- A PARTIR DE AQUI TENEMOS!!----
+--- COMUNICACION SERIAL
+--- TRAEMOS LA LECTURA SERIAL DE ARDUINO A NODE JS
+---
+*/
 const serialport= require("serialport");
 const parser= serialport.parsers.Readline// readline para tener cada lectura en una nueva linea 
 const puerto = new serialport("COM3",{
